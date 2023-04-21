16dad5796314098b9648c50b0de1cf359bc7ffe7
Chosso <kurotak7255@gmail.com>
2023-04-21 19:55:45 +0900

    エラトステネスのふるい追加

--- よく使う関数/sample.py
+++ よく使う関数/sample.py
@@ -55,3 +55,14 @@ class unionfind:
 	#  要素 u と v が同一のグループかどうかを返す関数
 	def same(self, u, v):
 		return self.root(u) == self.root(v)
+
+# エラトステネスのふるい
+# O(NloglogN)
+def eratosthenes_sieve(n):
+    is_prime = [True]*(n + 1)
+    is_prime[0] = is_prime[1] = False
+    for p in range(2, n + 1):
+        if is_prime[p]:
+            for q in range(2*p, n + 1, p):
+                is_prime[q] = False
+    return is_prime
