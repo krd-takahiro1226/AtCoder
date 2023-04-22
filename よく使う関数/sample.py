f670d96d61ec7cb271aedab68f6c2473ce5bc82b
Chosso <kurotak7255@gmail.com>
2023-04-22 19:19:29 +0900

    累乗関数を追加

--- よく使う関数/sample.py
+++ よく使う関数/sample.py
@@ -72,3 +72,14 @@ def Base_10_to_n(X, n):
     if X//n:
         return Base_10_to_n(X//n, n)+str(X % n)
     return str(X % n)
+
+# a の b 乗を m で割った余りを返す関数
+def Power(a, b, m):
+	p = a
+	Answer = 1
+	for i in range(30):
+		wari = 2 ** i
+		if (b // wari) % 2 == 1:
+			Answer = (Answer * p) % m # a の 2^i 乗が掛けられるとき
+		p = (p * p) % m
+	return Answer
