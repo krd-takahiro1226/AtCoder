2277134d98893f875ec6d11442b230069f26ffbb
Chosso <kurotak7255@gmail.com>
2023-04-29 20:08:29 +0900

    典型90 7

--- 競プロ典型90問/007 - CP Classes.py
+++ 競プロ典型90問/007 - CP Classes.py
@@ -1,6 +1,21 @@
+import bisect
 N = int(input())
 A = list(map(int, input().split()))
 Q = int(input())
-B = []
+A.sort()
+# print(A)
+# なぜかREが起きる →  bisect.bisect_leftを使用してright_indを求めているが、BがAのどの要素よりも大きい時、一番右に入ることになる。
+# その時、right_ind = Nとなるため、ifで条件分岐しないとREになる。
 for _ in range(Q):
-    B.append(int(input()))
+    B = int(input())
+    if N != 1:
+        left_ind = bisect.bisect_left(A, B) - 1
+        right_ind = bisect.bisect_left(A, B)
+        if right_ind <= N - 1:
+            diff = min(abs(B - A[left_ind]), abs(B - A[right_ind]))
+        else:
+            diff = abs(B - A[left_ind])
+        print(diff)
+    else:
+        diff = abs(B - A[0])
+        print(diff)
