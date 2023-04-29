4a9801c13fe6695fac81b342dee463a97957769c
Chosso <kurotak7255@gmail.com>
2023-04-29 20:08:43 +0900

    深さ優先探索追加

--- よく使う関数/sample.py
+++ よく使う関数/sample.py
@@ -19,42 +19,50 @@ def DFS(i):
             continue
         DFS(j - 1)
 
+# 鉄則本のDFS
+# 深さ優先探索を行う関数（pos は現在位置、visited[x] は頂点 x が青色かどうかを表す真偽値）
+def dfs(pos, G, visited):
+    visited[pos] = True
+    for i in G[pos]:
+        if visited[i] == False:
+            dfs(i, G, visited)
+
 
 # Union-Find 木
 class unionfind:
-	# n 頂点の Union-Find 木を作成
-	# （ここでは頂点番号が 1-indexed になるように実装しているが、0-indexed の場合は par, size のサイズは n でよい）
-	def __init__(self, n):
-		self.n = n
-		self.par = [ -1 ] * (n + 1) # 最初は親が無い
-		self.size = [ 1 ] * (n + 1) # 最初はグループの頂点数が 1
-	
-	# 頂点 x の根を返す関数
-	def root(self, x):
-		# 1 個先（親）がなくなる（つまり根に到達する）まで、1 個先（親）に進み続ける
-		while self.par[x] != -1:
-			x = self.par[x]
-		return x
-	
-	# 要素 u, v を統合する関数
-	def unite(self, u, v):
-		rootu = self.root(u)
-		rootv = self.root(v)
-		if rootu != rootv:
-			# u と v が異なるグループのときのみ処理を行う
-			if self.size[rootu] < self.size[rootv]:
-				self.par[rootu] = rootv
-				self.size[rootv] += self.size[rootu]
-			else:
-				self.par[rootv] = rootu
-				self.size[rootu] += self.size[rootv]
-				
-	def members(self, x):
-		roots = self.root(x)
-		return [i for i in range(self.n) if self.root(i) == roots]
-	#  要素 u と v が同一のグループかどうかを返す関数
-	def same(self, u, v):
-		return self.root(u) == self.root(v)
+    # n 頂点の Union-Find 木を作成
+    # （ここでは頂点番号が 1-indexed になるように実装しているが、0-indexed の場合は par, size のサイズは n でよい）
+    def __init__(self, n):
+        self.n = n
+        self.par = [ -1 ] * (n + 1) # 最初は親が無い
+        self.size = [ 1 ] * (n + 1) # 最初はグループの頂点数が 1
+        
+    # 頂点 x の根を返す関数
+    def root(self, x):
+        # 1 個先（親）がなくなる（つまり根に到達する）まで、1 個先（親）に進み続ける
+        while self.par[x] != -1:
+            x = self.par[x]
+        return x
+        
+    # 要素 u, v を統合する関数
+    def unite(self, u, v):
+        rootu = self.root(u)
+        rootv = self.root(v)
+        if rootu != rootv:
+            # u と v が異なるグループのときのみ処理を行う
+            if self.size[rootu] < self.size[rootv]:
+                self.par[rootu] = rootv
+                self.size[rootv] += self.size[rootu]
+            else:
+                self.par[rootv] = rootu
+                self.size[rootu] += self.size[rootv]
+                
+    def members(self, x):
+        roots = self.root(x)
+        return [i for i in range(self.n) if self.root(i) == roots]
+    #  要素 u と v が同一のグループかどうかを返す関数
+    def same(self, u, v):
+        return self.root(u) == self.root(v)
 
 # エラトステネスのふるい
 # O(NloglogN)
@@ -83,3 +91,36 @@ def Power(a, b, m):
 			Answer = (Answer * p) % m # a の 2^i 乗が掛けられるとき
 		p = (p * p) % m
 	return Answer
+
+
+# 深さ優先探索(鉄則本A63)
+import sys
+from collections import deque
+sys.setrecursionlimit(100000)
+N , M = map(int,input().split())
+# グラフ受け取り変数(1オリジン)
+graph = [[] for _ in range(N+1)]
+# -1で初期化
+dist = [-1] * (N+1)
+dist[1] = 0
+Q = deque()
+Q.append(1)
+
+# グラフの受け取り(1オリジン受け取り)
+for _ in range(M):
+    i, j = map(int,input().split())
+    graph[i].append(j)
+    graph[j].append(i)
+
+while len(Q) >= 1:
+# 初めは1(スタート地点)
+    pos = Q.popleft()
+# スタート地点と連結している場所1つ1つに対してfor文を行う
+    for i in graph[pos]:
+# distに既に値が入ってた場合、-1以外が入っている。dist = -1を満たさない場合は上書きしてしまうから実行しない。
+        if dist[i] == -1:
+            dist[i] = dist[pos] + 1
+            Q.append(i)
+
+for i in range(1,N+1):
+    print(dist[i])
