c949b0b2f608e7ed92c299923f7490fc6337d4aa
Chosso <kurotak7255@gmail.com>
2023-06-04 18:24:10 +0900

    ABC286 C

--- ABC286/C.py
+++ ABC286/C.py
@@ -1,2 +1,23 @@
-N ,A, B = map(int,input().split())
-S = input()
+N , A, B = map(int,input().split())
+S = list(input())
+j_cnt = 0 
+# 一致していない箇所の数を格納
+# 1要素目はシフト0
+cnt_list = []
+value_list = []
+
+for shift in range(N//2 + 1):
+    cnt = 0
+    for i in range(N//2):
+        if S[(i+shift)%N] != S[(N-i-1+shift)%N]:
+            cnt += 1
+    cnt_list.append(cnt)
+# print(cnt_list)
+
+for j in cnt_list:
+    value = 0
+    value += j * B + j_cnt * A
+    j_cnt += 1
+    value_list.append(value)
+
+print(min(value_list))
