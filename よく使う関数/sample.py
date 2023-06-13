1c0f0acfcd1c56a115889584778ee25264b592bc
Chosso <kurotak7255@gmail.com>
2023-06-13 20:42:49 +0900

    ランレングス圧縮を追加

--- よく使う関数/sample.py
+++ よく使う関数/sample.py
@@ -223,3 +223,16 @@ def index_of_permutation(L):
         index = index + a * factorial(len(L) - 1)
         L = L[1:]
     return index
+
+# ランレングス圧縮
+def rle(s):
+    n = len(s) #文字列の長さ
+    ans = [] #圧縮後のリスト
+    l = 0 #始点
+    while l<n:
+        r = l+1
+        while r<n and s[l]==s[r]: #異なる文字になるまで進む
+            r += 1
+        ans.append((s[l], r-l)) #文字,連続する個数
+        l = r #連続しなかった文字から探索を開始
+    return ans
